1d89a253bf8715ed695191a946a0ebf0de43887d
Riccardo Binetti <punkerbino@gmail.com>
2015-01-23 00:12:19 +0100

    Added aes128 without sboxes as alternative intermediate values

---
+++ src/gen_intermediate_prog/aes128round1_prog_nosbox.cpp
@@ -0,0 +1,76 @@
+/*
+Copyright (C) 2014 Riccardo Binetti
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+#include "dpacalc.h"
+#include "aes128round1_prog_nosbox.hpp"
+#include "aes.h"
+void GenerateIntermediateValuesProg::aes128round1_prog_nosbox::init()
+{
+	if ( whichsboxArg.getValue() + sboxnumArg.getValue() > AES_STATE_BYTES_NO || whichsboxArg.getValue() < 0 || sboxnumArg.getValue() < 1 ) {
+		cerr << "Maximum number of SBOXes exceeded." << endl;
+		exit ( 1 );
+	}
+}
+
+void GenerateIntermediateValuesProg::aes128round1_prog_nosbox::fill ( shared_ptr<DataMatrix>& knowndata, shared_ptr<IntermediateValueMatrix>& intval, unsigned long startTrace, unsigned int step )
+{
+	std::bitset<KEY_SIZE_BIT> key;
+	uint8_t fullaeskey[AES_STATE_BYTES_NO];
+	uint8_t fullaesdata[AES_STATE_BYTES_NO];
+	memset ( fullaeskey, 0, AES_STATE_BYTES_NO );
+	uint8_t UnrolledRoundKey[AES_128_ROUND_KEY_NO][AES_STATE_BYTES_NO];
+	uint8_t* dataptr = &fullaesdata[whichsboxArg.getValue()];
+	for ( KeyIndexType keyidx = 0; keyidx < KEYNUM ; ++keyidx ) {
+		key = keygen->getKeyFromIndex ( keyidx );
+		BitsetToBuffer<KEY_SIZE_BYTE> ( key, ( char* ) &fullaeskey );
+		KeyExpansion ( fullaeskey, UnrolledRoundKey );
+        for ( unsigned long trcidx = startTrace; trcidx < startTrace+step; trcidx++ ) {
+			BitsetToBuffer<DATA_SIZE_BYTE> ( ( *knowndata ) [trcidx], ( char* ) &fullaesdata );
+			AddRoundKey ( fullaesdata, UnrolledRoundKey[0] );
+			( *intval ) ( trcidx, keyidx ) = 0;
+			switch ( sboxnumArg.getValue() ) {
+				case 8:
+					( *intval ) ( trcidx, keyidx ) += ( ( IntermediateValueType ) ( * ( dataptr + 7 ) ) ) << 56;
+				case 7:
+					( *intval ) ( trcidx, keyidx ) += ( ( IntermediateValueType ) ( * ( dataptr + 6 ) ) ) << 48;
+				case 6:
+					( *intval ) ( trcidx, keyidx ) += ( ( IntermediateValueType ) ( * ( dataptr + 5 ) ) ) << 40;
+				case 5:
+					( *intval ) ( trcidx, keyidx ) += ( ( IntermediateValueType ) ( * ( dataptr + 4 ) ) ) << 32;
+				case 4:
+					( *intval ) ( trcidx, keyidx ) += ( ( IntermediateValueType ) ( * ( dataptr + 3 ) ) ) << 24;
+				case 3:
+					( *intval ) ( trcidx, keyidx ) += ( ( IntermediateValueType ) ( * ( dataptr + 2 ) ) ) << 16;
+				case 2:
+					( *intval ) ( trcidx, keyidx ) += ( ( IntermediateValueType ) ( * ( dataptr + 1 ) ) ) << 8;
+				case 1:
+					( *intval ) ( trcidx, keyidx ) += ( ( IntermediateValueType ) ( * ( dataptr ) ) );
+			}
+		}
+	}
+}
+
+void GenerateIntermediateValuesProg::aes128round1_prog_nosbox::progressiveGenerate ( shared_ptr<DataMatrix>& knowndata, shared_ptr<IntermediateValueMatrix>& intval, unsigned int step )
+{
+    if (intval.get() == NULL){
+        intval.reset( new IntermediateValueMatrix ( step, KEYNUM ));
+        fill(knowndata,intval,0,step);
+        return;
+    }
+    unsigned long curTraces = intval->rows();
+    intval->conservativeResize(curTraces+step,NoChange);
+    fill(knowndata, intval, curTraces, step);
+}
---
+++ src/gen_intermediate_prog/aes128round1_prog_nosbox.hpp
@@ -0,0 +1,41 @@
+/*
+Copyright (C) 2014 Riccardo Binetti
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+#pragma once
+#include "base.hpp"
+using namespace Eigen;
+using namespace std;
+namespace GenerateIntermediateValuesProg
+{
+    class aes128round1_prog_nosbox : public base
+	{
+		public:
+
+            virtual void progressiveGenerate ( shared_ptr<DataMatrix>& knowndata, shared_ptr<IntermediateValueMatrix>& intval, unsigned int step );
+			virtual void init();
+            aes128round1_prog_nosbox ( TCLAP::CmdLine& cmd, shared_ptr<KeyGenerators::base> _keygen ) : base ( cmd, _keygen ),
+				whichsboxArg ( "b", "sbox", "From which SBOX output should I start to correlate?", false, 0, "0-15" ),
+				sboxnumArg ( "v", "sboxnum", "How many consecutive SBOXes should I consider?", false, 1, "1-8" ) {
+				cmd.add ( whichsboxArg );
+				cmd.add ( sboxnumArg );
+			}
+		protected:
+            virtual void fill ( shared_ptr<DataMatrix>& knowndata, shared_ptr<IntermediateValueMatrix>& intval, unsigned long startTrace, unsigned int step );
+			TCLAP::ValueArg<int> whichsboxArg;
+			TCLAP::ValueArg<int> sboxnumArg;
+	};
+}
+
